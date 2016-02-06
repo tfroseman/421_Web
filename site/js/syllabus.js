74b30a572f77f650e55e1291417c27f1f6806eb5
Thomas Roseman <roseman.tom@gmail.com>
2016-02-05 22:14:36 -0500

    See todo but now has a working table of contents

---
+++ site/js/syllabus.js
@@ -0,0 +1,27 @@
+/**
+ * Created by thomasroseman on 2/5/16.
+ */
+
+function createTableofContents(){
+    // WHY IS THE PAGE LOADED IN AN IFRAME!?!?!?!?
+    //TODO Eveything is in one line
+    //TODO the last element is double diplayed.
+    //TODO All links are at the bottom of the page.
+    //
+    //An array of all the headings from the page
+    var iframe = document.getElementById('cframe');
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var headings = innerDoc.getElementsByTagName("h1");
+    var anchor = document.createElement("a");
+
+
+    //For each item insert a new link in the page that points to the heading href #
+    for (var i = 0; i < headings.length; i++) {
+        var anchor = document.createElement("a");
+        anchor.setAttribute("href", "#"+headings[i].nextSibling.nextSibling.id);
+        //innerDoc.getElementsByTagName("h1")[0].nextSibling.nextSibling.id
+        anchor.innerHTML = headings[i].innerHTML;
+        console.log(anchor);
+        innerDoc.body.insertBefore(anchor ,innerDoc.getElementsByTagName("pageTitleHeader")[0]);
+    }
+}
