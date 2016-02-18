f2e5a3f21bf903d37adc1c1c38bf894d2d476429
Thomas Roseman <roseman.tom@gmail.com>
2016-02-18 10:46:18 -0500

    Questions formatted and ready for move to template when needed

---
+++ site/EvalTool/evaltool.js
@@ -0,0 +1,16 @@
+function checkanswers(clientanswers){
+
+}
+
+var correctanswers = {
+    "question1":{"answer":1},
+    "question2":{"answer":1},
+    "question3":{"answer":3},
+    "question4":{"answer":2},
+    "question5":{"answer":2},
+    "question6":{"answer":2},
+    "question7":{"answer":3},
+    "question8":{"answer":1},
+    "question9":{"answer":1},
+    "question10":{"answer":3}
+};
