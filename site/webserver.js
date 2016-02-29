d929ed3995ce8026ee40de2733b1117ed32bf496
Thomas Roseman <roseman.tom@gmail.com>
2016-02-29 15:25:25 -0500

    More jade changes

--- site/webserver.js
+++ site/webserver.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({
 // Used to allow express to find the js file
 app.use(express.static('js'));
 app.use(express.static('css'));
+app.use(express.static('img'));
 
 // Set some express variables
 app.set("jsonp callback", true); //Allow for jsonp responses
@@ -29,32 +30,7 @@ app.set('view engine', 'jade'); // Using jade templates
 
 // Return default index
 app.get('/', function (req, res) {
-    fs.readFile('home.html', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        res.send(data);
-    });
-});
-
-// About.html page controller
-app.get('/about.html', function (req, res) {
-    fs.readFile('about.html', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        res.send(data);
-    });
-});
-
-// Generic side controller
-app.get('/side.html', function (req, res) {
-    fs.readFile('side.html', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        res.send(data);
-    });
+    res.render('About/home');
 });
 
 
@@ -121,31 +97,9 @@ app.route('/EvalTool/:side?')
         }
     });
 
-
-
-/**app.get('/Syllabus/syllabus.html', function(req, res){
-	fs.readFile('./Syllabus/syllabus.html', 'utf8', function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		res.send(data);
-	});
-});*/
-
-app.get('/ThreeRegion/*', threeregion);
-function threeregion(req, res) {
-    var fileName = root + req.path;
-    res.sendFile(fileName, function (err) {
-        if (err) {
-            console.log(err);
-            res.status(err.status).end();
-        }
-        else {
-            console.log('Sent:', req.path);
-        }
-    });
-}
-app.get('/CloudChat/*', ChatServer.gettool);
+app.route('/chat').get(function(req,res){
+    res.render('CloudChat/index');
+});
 app.get('/Syllabus/*', syllabus.gettool);
 
 app.listen(8080, function () {
